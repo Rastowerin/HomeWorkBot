48ab7d63b34b40248fa1aa3395a01806766cbd7a
Rost <rastowerin@gmail.com>
2019-01-11 15:06:28 +0300

    update 3 вроде запилил сохранение в файл

--- function.py
+++ function.py
@@ -1,13 +1,8 @@
 import vk_api
 import random
+import codecs
+import json
 from config import *
-
-vk_bot = vk_api.VkApi(token=ACCESS_TOKEN)
-long_poll = vk_bot.method('messages.getLongPollServer', {'need_pts': 1, 'lp_version': 3})
-server, key, ts = long_poll['server'], long_poll['key'], long_poll['ts']
-homework = {'алгебра': None, 'геометрия': None, 'Физика': None, 'химия': None, 'русский язык': None, 'английский язык': None, 'информатика': None, 'литература': None, 'музыка': None, 'изо': None, 'физ-ра': None, 'география': None, 'биология': None, 'история': None, 'обществознание': None}
-
-print('готов к работе')
 
 def write_msg(user_id, text):
     vk_bot.method('messages.send', {'user_id': user_id, 'message': text, 'random_id': random.randint(0, 1000)})
@@ -18,3 +13,23 @@ def write_msg_attach(user_id, text, att_url):
                    'attachment': att_url,
                    'message': text,
                    'random_id': random.randint(0, 1000)})
+
+def homework_file():
+    with codecs.open("C:\\Users\\Rost\\PycharmProjects\\HomeWorkBot\\subjects.txt", "r", "utf-8-sig") as json_data:
+        data = json.load(json_data)
+        return data
+
+def homework_new_file(subject, new_homework):
+    with codecs.open("C:\\Users\\Rost\\PycharmProjects\\HomeWorkBot\\subjects.txt", "w", "utf-8-sig") as test:
+        homework = homework_file()
+        homework[subject] = new_homework
+        homework = str(homework).replace('\'', '\"')
+        test.write(homework)
+        test.close()
+
+vk_bot = vk_api.VkApi(token=ACCESS_TOKEN)
+long_poll = vk_bot.method('messages.getLongPollServer', {'need_pts': 1, 'lp_version': 3})
+server, key, ts = long_poll['server'], long_poll['key'], long_poll['ts']
+homework = homework_file()
+
+print('готов к работе')
--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import datetime
 
 d = datetime.date.today()
 tomm = datetime.datetime.isoweekday(d) + 1
+homework = function.homework_file()
+data = function.homework_file()
 
 while True:
     long_poll = requests.get(
@@ -19,17 +21,14 @@ while True:
                 function.write_msg(update[0][3], function.homework['%s' % subject])
         elif update[0][0] == 4 and update[0][3] == 321056236 and '!!дз' in update[0][6] and update[0][6] != '!!дз':
             subject = update[0][6].split(': ')[1]
+            homework = update[0][6].split(': ')[2]
             print(subject)
-            if '%s' % subject in function.homework:
-                function.homework['%s' % subject] = update[0][6].split(': ')[2]
-                print(function.homework['%s' % subject])
+            if '%s' % subject in data:
+                function.homework_new_file(subject, homework)
+                print([subject])
         print(update)
         user_id = update[0][3]
         user_name = function.vk_bot.method('users.get', {'user_ids': user_id})
-        if 'привет' in update[0][6]:
-            function.write_msg(user_id, 'здоров, ' + (user_name[0]['first_name']))  # cooбщение пользователю
-        print(str(user_name[0]['first_name']) + ' ' +
-              str(user_name[0]['last_name']) + ' написал(а) боту - ' + str(update[0][6]))  # cooбщение пользователя
         if 'расписание' in update[0][6]:
             if tomm == 8:
                 function.write_msg(user_id, 'Технология, '
