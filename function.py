e36dfde0f07bb208ae296cebeb6d79c55871b40d
Rost <Rastowerin@gmail.com>
2019-04-28 15:06:12 +0300

    перенастройка под группы

--- function.py
+++ function.py
@@ -15,7 +15,7 @@ def write_msg_attach(user_id, text, att_url):
                    'random_id': random.randint(0, 1000)})
 
 def homework_file():
-    with codecs.open("C:\\Users\\Rost\\PycharmProjects\\HomeWorkBot\\subjects.txt", "r", "utf-8-sig") as json_data:
+    with codecs.open("subjects.txt", "r", "utf-8-sig") as json_data:
         data = json.load(json_data)
         return data
 
@@ -23,13 +23,13 @@ def homework_new_file(subject, new_homework):
     homework = homework_file()
     homework[subject] = new_homework
     homework = str(homework).replace('\'', '\"')
-    with codecs.open("C:\\Users\\Rost\\PycharmProjects\\HomeWorkBot\\subjects.txt", "w", "utf-8-sig") as test:
+    with codecs.open("subjects.txt", "w", "utf-8-sig") as test:
         test.write(homework)
         test.close()
 
-vk_bot = vk_api.VkApi(token=ACCESS_TOKEN)
-long_poll = vk_bot.method('messages.getLongPollServer', {'need_pts': 1, 'lp_version': 3})
+vk_bot = vk_api.VkApi(token=TOKEN)
+long_poll = vk_bot.method('groups.getLongPollServer', {'group_id': 181347142, 'lp_version': 3})
 server, key, ts = long_poll['server'], long_poll['key'], long_poll['ts']
 homework = homework_file()
 
-print('готов к работе')
+print('HomeWorkBot is online')
--- main.py
+++ main.py
@@ -1,22 +1,22 @@
 import requests
 import function
-import datetime
+from time import sleep
 
-d = datetime.date.today()
-tomm = datetime.datetime.isoweekday(d) + 1
 homework = function.homework_file()
 data = function.homework_file()
 print(data)
 
 while True:
-    new_ts = function.vk_bot.method('messages.getLongPollServer', {'need_pts': 1, 'lp_version': 3})
+    sleep(1)
+    new_ts = function.vk_bot.method('groups.getLongPollServer', {'group_id': 181347142, 'lp_version': 3})
     function.ts = new_ts['ts']
     long_poll = requests.get(
-        'https://{server}?act={act}&key={key}&ts={ts}&wait=15000'.format(server=function.server,
+        '{server}?act={act}&key={key}&ts={ts}&wait=15000'.format(server=function.server,
                                                                        act='a_check',
                                                                        key=function.key,
                                                                        ts=function.ts)).json()
     update = long_poll['updates']
+    print(update)
     if update[0][0] == 4:
         if update[0][0] == 4 and '!дз' in update[0][6] and '!!дз' not in update[0][6] and update[0][6] != '!дз':
             subject = update[0][6].split(': ')[1]
@@ -34,49 +34,3 @@ while True:
                 print(0)
         user_id = update[0][3]
         user_name = function.vk_bot.method('users.get', {'user_ids': user_id})
-        if 'расписание' in update[0][6]:
-            if tomm == 8:
-                function.write_msg(user_id, 'Технология, '
-                                   'география, '
-                                   'алгебра, '
-                                   'физика, '
-                                   'химия, '
-                                   'физра')
-            elif tomm == 2:
-                function.write_msg(user_id, 'Литра, '
-                                   'черчение, '
-                                   'общество, '
-                                   'инглиш, '
-                                   'биология, '
-                                   'геометрия, '
-                                   'кл. час')
-            elif tomm == 3:
-                function.write_msg(user_id, 'Русский, '
-                                   'инглиш, '
-                                   'физра, '
-                                   'история, '
-                                   'алгебра, '
-                                   'химия')
-            elif tomm == 4:
-                function.write_msg(user_id, 'Физика, '
-                                   'география, '
-                                   'русский, '
-                                   'инглиш, '
-                                   'обж, '
-                                   'геометрия')
-            elif tomm == 5:
-                function.write_msg(user_id, 'Музыка, '
-                                   'изо, '
-                                   'инфа, '
-                                   'биология, '
-                                   'русский, '
-                                   'история')
-            elif tomm == 6:
-                function.write_msg(user_id, 'Литра, '
-                                   'геометрия, '
-                                   'физра, '
-                                   'история, '
-                                   'спб, '
-                                   'алгебра')
-            elif tomm == 7:
-                function.write_msg(user_id, 'завтра мы не учимся')
