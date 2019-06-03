493e1e7f22e238f8761e289edd8d798c35e57257
rostislav <rastowerin@gmail.com>
2019-06-03 18:24:29 +0300

    ы

--- main.py
+++ main.py
@@ -8,18 +8,23 @@ keyboard_status = {}
 
 while True:
     try:
-        function.time_check()
         homework = function.homework_file()
         data = function.homework_file()
         new_ts = function.vk_bot.method('groups.getLongPollServer', {'group_id': 181347142, 'lp_version': 3})
         ts = new_ts['ts']
         long_poll = requests.get('%s?act=%s&key=%s&ts=%s&wait=15000' % (server, 'a_check', key, ts)).json()
         update = long_poll['updates']
+        #function.time_check()
         for element in update:
             if element['type'] == 'message_new':
                 user = function.vk_bot.method('users.get', {'user_ids': element['object']['from_id']})
                 print('%s %s %s: %s' % (str(d.datetime.today())[10: 19], user[0]['first_name'], user[0]['last_name'], element['object']['text']))
-                if element['object']['text'] == 'получить дз':
+                if element['object']['from_id'] not in keyboard_status:
+                    print('%s' % element['object']['from_id'])
+                    print(keyboard_status)
+                    keyboard_status['%s' % element['object']['from_id']] == 0
+                    function.write_msg(element['object']['from_id'], 'ты добавлен в список пользователей', function.start_keyboard)
+                elif element['object']['text'] == 'получить дз':
                     function.write_msg(element['object']['from_id'], 'выбери предмет', function.choice_keyboard)
                     keyboard_status['%s' % element['object']['from_id']] = 0
                     print('%s HomeWorkBot: выбери предмет' % str(d.datetime.today())[10: 19])
@@ -31,7 +36,7 @@ while True:
                     keyboard_status['%s' % element['object']['from_id']] = 1
                 elif element['object']['text'] in data:
                     if keyboard_status['%s' % element['object']['from_id']] == 0:
-                        function.write_msg(element['object']['from_id'], homework['%s' % element['object']['text']][0], None)
+                        function.write_msg(element['object']['from_id'], homework['%s' % element['object']['text']], None)
                         print('%s HomeWorkBot: %s' % (str(d.datetime.today())[10: 19], homework['%s' % element['object']['text']]))
                     elif keyboard_status['%s' % element['object']['from_id']] == 1:
                         function.write_msg(element['object']['from_id'], 'старое значение: %s' % homework['%s' % element['object']['text']], None)
@@ -41,8 +46,8 @@ while True:
                         keyboard_status['%s' % element['object']['from_id']] = 2
                     else:
                         None
-                elif keyboard_status['%s' % element['object']['from_id']] == 2:
-                    function.homework_new_file(subject, [element['object']['text'], '%s' % d.datetime.today()])
+                elif element['object']['from_id'] in keyboard_status and keyboard_status['%s' % element['object']['from_id']] == 2:
+                    function.homework_new_file(subject, element['object']['text'])
                     function.write_msg(element['object']['from_id'], 'значение обновлено', function.start_keyboard)
                     print('%s HomeWorkBot: значеие обновлено' % str(d.datetime.today())[10: 19])
                     print('value updated: %s: %s' % (subject, element['object']['text']))
@@ -50,14 +55,14 @@ while True:
                 else:
                     function.write_msg(element['object']['from_id'], 'неизвестная команда', function.start_keyboard)
                     print('%s HomeWorkBot: неизвестная команда' % str(d.datetime.today())[10: 19])
-                if function.day != print(d.datetime.today().weekday()):
-                    function.day == d.datetime.today().weekday()
-                    with codecs.open("schedule.txt", "w", "utf-8-sig") as file:
-                        file['day'] = '%s' % d.datetime.today().weekday()
-                        file.write()
-                        file.close()
+                #if function.day != print(d.datetime.today().weekday()):
+                 #   function.day == d.datetime.today().weekday()
+                  #  with codecs.open("schedule.txt", "w", "utf-8-sig") as file:
+                   #     file['day'] = '%s' % d.datetime.today().weekday()
+                    #    file.write(str(file))
+                     #   file.close()
 
-    except KeyError:
+    except ZeroDivisionError:
         if long_poll == {'failed': 2}:
             server, key, ts = function.long_poll()
         else:
